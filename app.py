7a1add7a94dbc9f6d3a6917d7ec77c7b04af2952
oleg-infinity <klimoleh045@gmail.com>
2025-11-08 00:44:40 +0100

    Your message

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import re
 from dotenv import load_dotenv
+import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -16,7 +17,8 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 csrf = CSRFProtect(app)
 csrf.init_app(app)
